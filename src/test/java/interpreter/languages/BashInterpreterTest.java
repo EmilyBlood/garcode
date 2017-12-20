90391fe343822b8d1ea0f921a15a940dee31bd30
Marcin Przewięźlikowski <m.przewie@gmail.com>
2017-12-20 20:23:54 +0100

    Instantiate interpreterDevelopment

--- src/test/java/interpreter/languages/BashInterpreterTest.java
+++ src/test/java/interpreter/languages/BashInterpreterTest.java
@@ -4,7 +4,6 @@ import exerciseCreator.databaseProvider.entity.TestCase;
 import interpreter.Result;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
