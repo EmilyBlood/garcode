fea64f2e2adb0a2896338b5c95126d34abfaa705
mdarul@outlook.com <mdarul618@gmail.com>
2018-01-09 21:27:16 +0100

    sms notifications with simple test

--- src/main/java/notifications/Executor.java
+++ src/main/java/notifications/Executor.java
@@ -1,3 +1,6 @@
+/**
+ * Created by Michał on 19.12.2017.
+ */
 package notifications;
 
 import java.io.IOException;
@@ -7,12 +10,18 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-/**
- * Created by Michał on 19.12.2017.
- */
+
+import exerciseCreator.Outcome;
+
 public class Executor {
-    public static void main(String[] args) {
-        //String hostMail = "mdarul618@gmail.com";
+
+    public static void main(String[] args) throws Exception {
+        SmsSender smsSender = new SmsSender();
+        Outcome outcome = new Outcome("10", "Adam", "Adamiak", "726498829", "test@test.com", "Excellent", "Title", 16, 20);
+        smsSender.sendResults(outcome);
+    }
+//    public static void main(String[] args) {
+//        String hostMail = "mdarul618@gmail.com";
 //        String subject = "testujemy123";
 //        String body = "burżuazja lorem ipsum test";
 //        Sender sender1 = new Sender(hostMail, subject, body);
@@ -22,5 +31,5 @@ public class Executor {
 //        MailConfiguration mailConfiguration = new MailConfiguration("grabowszczakls", "Test12345", "smtp.gmail.com", true, 587);
 //        Sender sender2 = new Sender(content, mailConfiguration); // Dependency injection
 //        sender2.sendFromGMail();
-    }
+//    }
 }
---
+++ src/test/java/notifications/SmsMessageComposerTest.java
@@ -0,0 +1,26 @@
+package notifications;
+
+import exerciseCreator.Outcome;
+
+import notifications.MessageComposers.SmsMessageComposer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by michal on 09/01/18.
+ */
+public class SmsMessageComposerTest {
+    Outcome outcome = new Outcome("10", "Adam", "Adamiak", "123456789", "test@test.com", "Excellent", "Title", 16, 20);
+    SmsMessageComposer smsMessageComposer;
+
+    @BeforeEach
+    void setUp() {smsMessageComposer = new SmsMessageComposer(outcome);}
+
+    @Test
+    void composeMessage() {
+        assertEquals("Adam Adamiak,\nOcena: 10,\n", smsMessageComposer.composeMessage());
+    }
+
+}
