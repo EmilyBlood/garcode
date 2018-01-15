60a8319977c11a7ee6df79a2d79ed22a75bead6d
Marcin Przewięźlikowski <m.przewie@gmail.com>
2018-01-15 09:51:19 +0100

    Merge InterpreterDevelopment into master

---
+++ src/test/java/interpreter/processing/ProcessWrapperTest.java
@@ -0,0 +1,18 @@
+package interpreter.processing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProcessWrapperTest {
+
+    // TODO tests that check exceptions thrown on execution
+    // TODO Mock a processbuilder
+    @Test
+    void getStdErr() {
+    }
+
+    @Test
+    void getExecutionTime() {
+    }
+}
